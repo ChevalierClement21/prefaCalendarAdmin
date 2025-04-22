dabfda5bfcdd78490377f40a426bcd6c9a70d811
Clément Chevalier <clementchevalier2102@outlook.fr>
2025-04-22 19:52:37 +0200

    Ajoutez des fichiers projet.

---
+++ prefaCalendarAdmin/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace prefaCalendarAdmin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
